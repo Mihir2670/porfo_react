3844ffd79c443af7a4a1e92a66b269543dba3171
Mihir2670 <mdalvi986@gmail.com>
2025-07-16 11:51:43 +0530

    Intial_commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import Navfunc from "@/component/func";
+export default function Home() {
+  return (
+    <Navfunc />
+  );
+}
---
+++ src/component/func.tsx
@@ -0,0 +1,13 @@
+export default function Navfunc(){
+ return(
+  <>
+    <h1>Nav bar</h1>
+    <ul>
+        <li>Home</li>
+        <li>About us</li>
+        <li>Project</li>
+        <li>Contact</li>
+    </ul>
+  </>
+ )
+}
