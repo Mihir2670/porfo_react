86b354a8d2478053b1096f975cd80832098ac5af
Mihir2670 <mdalvi986@gmail.com>
2025-07-16 21:24:43 +0530

    tailwind_css

--- src/component/func.tsx
+++ src/component/func.tsx
@@ -1,13 +1,14 @@
+import Link from "next/link";
+
 export default function Navfunc(){
  return(
-  <>
-    <h1>Nav bar</h1>
-    <ul>
-        <li>Home</li>
-        <li>About us</li>
-        <li>Project</li>
-        <li>Contact</li>
+  <nav>
+    <ul className="flex justify-center">
+      <Link href="/home" className="bg-sky-500/70 px-8 py-4 ">Home</Link>
+      <Link href="/about" className="bg-sky-500/70 px-8 py-4">About us</Link>
+      <Link href="/project" className="bg-sky-500/70 px-8 py-4">Project</Link>
+      <Link href="/contact" className="bg-sky-500/70 px-8 py-4">Contact</Link>
     </ul>
-  </>
+  </nav>
  )
 }
